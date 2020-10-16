2b5207564ad04d6a567a54ba3af3848e37a41480
Emmanuel Paniagua <emmanuel.paniagua4671@alumnos.udg.mx>
2020-10-16 14:08:01 -0500

    agregamos def __str, ahora imprime datos de prueba

--- particula.py
+++ particula.py
@@ -13,4 +13,22 @@ class Particula:
         self.__green = green
         self.__blue = blue
         self.__distancia = distancia
-        
+
+    def __str__(self):
+        return (
+            'Id:  ' + str(self.__id) + '\n' +
+            'Origen x:  ' + str(self.__origen_x) + '\n' +
+            'Origen y:  ' + str(self.__origen_y) + '\n' +
+            'Destino x:  ' + str(self.__destino_x) + '\n' +
+            'Destino y:  ' + str(self.__destino_y) + '\n' +
+            'Velocidad:  ' + str(self.__velocidad) + '\n' +
+            'Red:  ' + str(self.__red) + '\n' +
+            'Green:  ' + str(self.__green) + '\n' +
+            'Blue:  ' + str(self.__blue) + '\n' +
+            'Distancia:  ' + str(self.__distancia) + '\n'
+            )
+
+l01 = Particula(id=2306, origen_x=34, origen_y=47, 
+                destino_x=90, destino_y=23, velocidad=80, red=90, 
+                green=2, blue=34, distancia=5.0)
+print(l01)
